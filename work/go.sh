d7315e953926070489cb1e68f860fdccf3083406
Dusan Holub <dusan.holub@gmail.com>
2026-02-07 09:46:36 +0100

    Premature exit if cmd failed

--- work/go.sh
+++ work/go.sh
@@ -6,7 +6,7 @@ rm *.com
 #./make_dist.sh
 #cd work
 
-zapc -6502 -I lib -o test_stdio.s test_stdio.zap
-ca65 test_stdio.s -o test_stdio.o
-ld65 -C ../cfg/my_atari.cfg  test_stdio.o -o test_stdio.com
+zapc -6502 -I lib -o test_stdio.s test_stdio.zap || exit
+ca65 test_stdio.s -o test_stdio.o || exit
+ld65 -C ../cfg/my_atari.cfg  test_stdio.o -o test_stdio.com || exit
 atari800 test_stdio.com
