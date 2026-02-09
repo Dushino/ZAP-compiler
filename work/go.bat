92e2d3214d61701ead26274efaf64973c432c65e
dusanholub <dusan.holub@advantech.cz>
2026-02-09 08:33:49 +0100

    Error handling

--- work/go.bat
+++ work/go.bat
@@ -2,7 +2,7 @@ del *.s
 del *.o
 del *.com
 
-zapc.exe -I lib -6502 -o test_stdio.s test_stdio.zap
-ca65 test_stdio.s -o test_stdio.o
-ld65 -C ..\cfg\my_atari.cfg  test_stdio.o -o test_stdio.com
-C:\Users\dusan.holub\local\Altirra\Altirra64.exe /w /basic /run test_stdio.com
+zapc.exe -O1 -I lib -6502 -o test_stdio.s test_stdio.zap  || exit /b
+ca65 test_stdio.s -o test_stdio.o  || exit /b
+ld65 -C ..\cfg\my_atari.cfg  test_stdio.o -o test_stdio.com  || exit /b
+C:\Users\dusan.holub\local\Altirra\Altirra64.exe /w /basic /run test_stdio.com 
