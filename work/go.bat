8e6fdd94952a61bc92873e3b19a30347b8297f72
dusanholub <dusan.holub@advantech.cz>
2026-02-09 10:52:38 +0100

    Now using current python module instead of compiled executable

--- work/go.bat
+++ work/go.bat
@@ -1,8 +1,10 @@
+@echo off
 del *.s
 del *.o
 del *.com
 
-zapc.exe -O1 -I lib -6502 -o test_stdio.s test_stdio.zap  || exit /b
+python ..\compiler.py -O1 -I lib -6502 -o test_stdio.s test_stdio.zap
+rem zapc.exe -O1 -I lib -6502 -o test_stdio.s test_stdio.zap  || exit /b
 ca65 test_stdio.s -o test_stdio.o  || exit /b
 ld65 -C ..\cfg\my_atari.cfg  test_stdio.o -o test_stdio.com  || exit /b
 C:\Users\dusan.holub\local\Altirra\Altirra64.exe /w /basic /run test_stdio.com 
