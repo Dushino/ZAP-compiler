cc2bb846221470078ce5bda4fca6ea02e4c962d0
Dusan Holub <dusan.holub@gmail.com>
2026-02-13 08:07:50 +0100

    Do not use binary of zapc

--- work/go.sh
+++ work/go.sh
@@ -6,7 +6,8 @@ rm *.com
 #./make_dist.sh
 #cd work
 
-zapc -6502 -I lib -o test_stdio.s test_stdio.zap || exit
+python3 ../compiler.py -O1 -I lib -6502 -o test_stdio.s test_stdio.zap || exit
+# zapc -6502 -I lib -o test_stdio.s test_stdio.zap || exit
 ca65 test_stdio.s -o test_stdio.o || exit
 ld65 -C ../cfg/my_atari.cfg  test_stdio.o -o test_stdio.com || exit
 atari800 test_stdio.com
