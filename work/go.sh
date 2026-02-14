7bc25f5d67f6466152a80476bfba69d40d864ea6
Dusan Holub <dusan.holub@gmail.com>
2026-02-14 23:21:19 +0100

    Removed 65c02 target

--- work/go.sh
+++ work/go.sh
@@ -7,7 +7,7 @@ rm *.com
 #cd work
 
 python3 ../compiler.py -O1 -I lib -6502 -o test_stdio.s test_stdio.zap || exit
-python3 ../compiler.py -O1 -I lib -o test_stdio_65c02.s test_stdio.zap || exit
+# python3 ../compiler.py -O1 -I lib -o test_stdio_65c02.s test_stdio.zap || exit
 # zapc -6502 -I lib -o test_stdio.s test_stdio.zap || exit
 ca65 test_stdio.s -o test_stdio.o || exit
 ld65 -C ../cfg/my_atari.cfg  test_stdio.o -o test_stdio.com || exit
