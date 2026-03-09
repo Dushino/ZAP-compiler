a728c0d41271ed2e734906c396042b83f6ff6e30
dusanholub <dusan.holub@advantech.cz>
2026-03-09 17:36:55 +0100

    Still no luck on autorun

--- work/temp/go.bat
+++ work/temp/go.bat
@@ -1,3 +1,3 @@
 ﻿rem dir2atr -b DOS25 my_disk.atr disk
 dir2atr -m -d -b MyDos455Beta4 disk.atr disk
-C:\Users\dusan.holub\local\Altirra\Altirra64.exe /w /nobasic disk.atr
+C:\Users\dusan.holub\local\Altirra\Altirra64.exe /w /basic disk.atr
