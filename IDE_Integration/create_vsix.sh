89a256e97d08d1a58de99fd24474dbf61d91ca35
Dusan Holub <dusan.holub@gmail.com>
2026-06-06 07:56:39 +0200

    vsix extension created

---
+++ IDE_Integration/create_vsix.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+mkdir -p ~/.npm-global
+npm config set prefix '~/.npm-global'
+export PATH="$HOME/.npm-global/bin:$PATH"
+source ~/.bashrc
+npm install -g @vscode/vsce
+
+cd dushino.zap-language
+npx @vscode/vsce package
+
