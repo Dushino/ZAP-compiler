7383de7edc4e32ee7274f5feb48510e92f028388
Dusan Holub <dusan.holub@gmail.com>
2026-06-22 06:54:37 +0200

    Fix: ASM - END block has not proper syntax highlighting

--- IDE_Integration/create_vsix.sh
+++ IDE_Integration/create_vsix.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
-sudo apt-get install -y nodejs
 
-mkdir -p ~/.npm-global
-npm config set prefix '~/.npm-global'
-export PATH="$HOME/.npm-global/bin:$PATH"
+# only first time
+#curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+#sudo apt-get install -y nodejs
+
+#mkdir -p ~/.npm-global
+#npm config set prefix '~/.npm-global'
+#export PATH="$HOME/.npm-global/bin:$PATH"
+#source ~/.bashrc
+#npm install -g @vscode/vsce
+# end of first time install only
+
 source ~/.bashrc
-npm install -g @vscode/vsce
-
 cd dushino.zap-language
 npx @vscode/vsce package
 
